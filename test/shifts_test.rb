84012660f9b899df90906d516672334bef1a881b
Sienna Kopf <princess.kopf@gmail.com>
2020-06-05 21:02:06 -0600

    Add seup and existance tests for shifts test class

---
+++ test/shifts_test.rb
@@ -0,0 +1,17 @@
+require './test/test_helper'
+require './lib/shifts'
+
+class ShiftsTest < Minitest::Test
+  def setup
+    @shift = Shifts.new({
+      :A => 3,
+      :B => 27,
+      :C => 73,
+      :D => 20
+      })
+  end
+
+  def test_it_exists
+    assert_instance_of Shifts, @shift 
+  end
+end
