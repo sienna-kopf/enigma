d70e4ab3f13a765b0f478582fc7496894197817b
Sienna Kopf <princess.kopf@gmail.com>
2020-06-05 21:11:21 -0600

    Edit format of shifts hash

--- test/shifts_test.rb
+++ test/shifts_test.rb
@@ -3,12 +3,7 @@ require './lib/shifts'
 
 class ShiftsTest < Minitest::Test
   def setup
-    @shift = Shifts.new({
-      :A => 3,
-      :B => 27,
-      :C => 73,
-      :D => 20
-      })
+    @shift = Shifts.new({"A" => 3, "B" => 27, "C" => 73, "D" => 20})
   end
 
   def test_it_exists
@@ -18,7 +13,7 @@ class ShiftsTest < Minitest::Test
   def test_it_has_A_B_C_D_shifts
     assert_equal 3, @shift.a_shift
     assert_equal 27, @shift.b_shift
-    assert_equal 73, @shift.c_shift 
+    assert_equal 73, @shift.c_shift
     assert_equal 20, @shift.d_shift
   end
 end
