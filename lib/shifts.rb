c286f6e3a982715103489fda3f86590b5237e3f6
Sienna Kopf <princess.kopf@gmail.com>
2020-06-05 21:21:25 -0600

    Add attr reader and instance variables to the shifts class

--- lib/shifts.rb
+++ lib/shifts.rb
@@ -1,5 +1,13 @@
 class Shifts
+  attr_reader :a_shift,
+                :b_shift,
+                :c_shift,
+                :d_shift
 
-  def initialize(shifts)
-  end
+    def initialize(shifts)
+      @a_shift = shifts["A"]
+      @b_shift = shifts["B"]
+      @c_shift = shifts["C"]
+      @d_shift = shifts["D"]
+    end
 end
