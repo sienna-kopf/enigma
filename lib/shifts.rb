5a0edd20dc5b97655c2a475ca7fb09b5d04ce64a
Sienna Kopf <princess.kopf@gmail.com>
2020-06-05 21:19:44 -0600

    Add shifts class setup to pass existance test

--- lib/shifts.rb
+++ lib/shifts.rb
@@ -1,13 +1,5 @@
 class Shifts
-  attr_reader :a_shift,
-              :b_shift,
-              :c_shift,
-              :d_shift
 
   def initialize(shifts)
-    @a_shift = shifts["A"]
-    @b_shift = shifts["B"]
-    @c_shift = shifts["C"]
-    @d_shift = shifts["D"]
   end
 end
