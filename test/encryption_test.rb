ffda956b31d7105d3bab9b1b299d2f77711627cc
Sienna Kopf <princess.kopf@gmail.com>
2020-06-07 00:33:11 -0600

    Add test for characer set

--- test/encryption_test.rb
+++ test/encryption_test.rb
@@ -8,6 +8,14 @@ class EncryptionTest < Minitest::Test
   end
 
   def test_it_exists
-    assert_instance_of Encryption, @encryption 
+    assert_instance_of Encryption, @encryption
+  end
+
+  def test_it_is_initialized_with_a_character_set
+    expected = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", " "]
+
+    assert_equal expected, @encryption.character_set
+    assert_equal 27, @encryption.character_set.count
+    assert_equal "a", @encryption.character_set[0]
   end
 end
