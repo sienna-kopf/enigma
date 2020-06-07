fb2540c33cc9573200c196bb117b98c3d6383b08
Sienna Kopf <princess.kopf@gmail.com>
2020-06-06 22:14:01 -0600

    modify shifts class to not take an argument and set keys offsets and shifts as instance variables instead

--- test/shifts_test.rb
+++ test/shifts_test.rb
@@ -3,18 +3,11 @@ require './lib/shifts'
 
 class ShiftsTest < Minitest::Test
   def setup
-    @shift = Shifts.new({"A" => 3, "B" => 27, "C" => 73, "D" => 20})
+    @shift = Shifts.new
   end
 
   def test_it_exists
     assert_instance_of Shifts, @shift
-  end
-
-  def test_it_has_A_B_C_D_shifts
-    assert_equal 3, @shift.a_shift
-    assert_equal 27, @shift.b_shift
-    assert_equal 73, @shift.c_shift
-    assert_equal 20, @shift.d_shift
   end
 
   def test_it_can_generate_a_random_number
