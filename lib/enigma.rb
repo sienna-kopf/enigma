cc7b8172bf7479cdbd812ff9e3299146cac42fac
Sienna Kopf <princess.kopf@gmail.com>
2020-06-08 16:24:43 -0600

    Update encrypt method to not transform given key to integer before use

--- lib/enigma.rb
+++ lib/enigma.rb
@@ -1,2 +1,18 @@
+require_relative './encryption'
+require_relative './shifts'
+
 class Enigma
-end 
+
+  def initialize
+    @encryption = Encryption.new
+    @shifts = Shifts.new
+  end
+
+  def encrypt(message, key, date)
+    {
+      :encryption => @encryption.encrypt_message(message, @shifts.shifts(key, date)),
+      :key => key,
+      :date => date
+    }
+  end
+end
