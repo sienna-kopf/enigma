193d4e85dfed4cc254cbb22cdbaf81b16103fdfd
Sienna Kopf <princess.kopf@gmail.com>
2020-06-07 23:16:03 -0600

    Add test for indexed_alphabet from character set

--- test/decryption_test.rb
+++ test/decryption_test.rb
@@ -19,4 +19,9 @@ class DecryptionTest < Minitest::Test
     assert_equal 27, @decryption.character_set.count
     assert_equal "a", @decryption.character_set[0]
   end
+
+  def test_it_can_create_an_index_to_letter_hash_for_character_set
+    expected = {0=>"a", 1=>"b", 2=>"c", 3=>"d", 4=>"e", 5=>"f", 6=>"g", 7=>"h", 8=>"i", 9=>"j", 10=>"k", 11=>"l", 12=>"m", 13=>"n", 14=>"o", 15=>"p", 16=>"q", 17=>"r", 18=>"s", 19=>"t", 20=>"u", 21=>"v", 22=>"w", 23=>"x", 24=>"y", 25=>"z", 26=>" "}
+    assert_equal expected, @decryption.indexed_alphabet
+  end
 end
