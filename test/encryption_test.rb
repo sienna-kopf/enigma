8102d819a098e8ee1ea56a9eae51864f3edbdc56
Sienna Kopf <princess.kopf@gmail.com>
2020-06-07 21:07:00 -0600

    Add test for indexing the alphabet into a hash

--- test/encryption_test.rb
+++ test/encryption_test.rb
@@ -20,7 +20,13 @@ class EncryptionTest < Minitest::Test
     assert_equal "a", @encryption.character_set[0]
   end
 
+  def test_it_can_create_an_index_to_letter_hash_for_character_set
+    expected = {0=>"a", 1=>"b", 2=>"c", 3=>"d", 4=>"e", 5=>"f", 6=>"g", 7=>"h", 8=>"i", 9=>"j", 10=>"k", 11=>"l", 12=>"m", 13=>"n", 14=>"o", 15=>"p", 16=>"q", 17=>"r", 18=>"s", 19=>"t", 20=>"u", 21=>"v", 22=>"w", 23=>"x", 24=>"y", 25=>"z", 26=>" "}
+    assert_equal expected, @encryption.indexed_alphabet
+  end
+
   def test_it_can_encrypt_a_message
+    skip 
     @shifts.stubs(:random_number).returns(2715)
     Date.stubs(:today).returns(Date.new(1995, 8, 04))
 
