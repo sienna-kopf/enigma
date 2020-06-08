06f487dac1aac594b08321798209b9248a0ef2b8
Sienna Kopf <princess.kopf@gmail.com>
2020-06-07 23:29:20 -0600

    Update decryption character set test and hash tests to accomodate reversed set

--- test/decryption_test.rb
+++ test/decryption_test.rb
@@ -13,20 +13,20 @@ class DecryptionTest < Minitest::Test
   end
 
   def test_it_is_initialized_with_a_character_set
-    expected = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", " "]
+    expected = [" ", "z", "y", "x", "w", "v", "u", "t", "s", "r", "q", "p", "o", "n", "m", "l", "k", "j", "i", "h", "g", "f", "e", "d", "c", "b", "a"]
 
     assert_equal expected, @decryption.character_set
     assert_equal 27, @decryption.character_set.count
-    assert_equal "a", @decryption.character_set[0]
+    assert_equal " ", @decryption.character_set[0]
   end
 
   def test_it_can_create_an_index_to_letter_hash_for_character_set
-    expected = {0=>"a", 1=>"b", 2=>"c", 3=>"d", 4=>"e", 5=>"f", 6=>"g", 7=>"h", 8=>"i", 9=>"j", 10=>"k", 11=>"l", 12=>"m", 13=>"n", 14=>"o", 15=>"p", 16=>"q", 17=>"r", 18=>"s", 19=>"t", 20=>"u", 21=>"v", 22=>"w", 23=>"x", 24=>"y", 25=>"z", 26=>" "}
+    expected = {0=>" ", 1=>"z", 2=>"y", 3=>"x", 4=>"w", 5=>"v", 6=>"u", 7=>"t", 8=>"s", 9=>"r", 10=>"q", 11=>"p", 12=>"o", 13=>"n", 14=>"m", 15=>"l", 16=>"k", 17=>"j", 18=>"i", 19=>"h", 20=>"g", 21=>"f", 22=>"e", 23=>"d", 24=>"c", 25=>"b", 26=>"a"}
     assert_equal expected, @decryption.indexed_alphabet
   end
 
   def test_it_can_create_a_letter_to_index_hash_for_character_set
-    expected = {"a"=>0, "b"=>1, "c"=>2, "d"=>3, "e"=>4, "f"=>5, "g"=>6, "h"=>7, "i"=>8, "j"=>9, "k"=>10, "l"=>11, "m"=>12, "n"=>13, "o"=>14, "p"=>15, "q"=>16, "r"=>17, "s"=>18, "t"=>19, "u"=>20, "v"=>21, "w"=>22, "x"=>23, "y"=>24, "z"=>25, " "=>26}
+    expected = {" "=>0, "z"=>1, "y"=>2, "x"=>3, "w"=>4, "v"=>5, "u"=>6, "t"=>7, "s"=>8, "r"=>9, "q"=>10, "p"=>11, "o"=>12, "n"=>13, "m"=>14, "l"=>15, "k"=>16, "j"=>17, "i"=>18, "h"=>19, "g"=>20, "f"=>21, "e"=>22, "d"=>23, "c"=>24, "b"=>25, "a"=>26}
     assert_equal expected, @decryption.alphabet_to_index
   end
 
