9651282c986954e51aebaa4a78efd45865aeb4ef
Sienna Kopf <princess.kopf@gmail.com>
2020-06-07 23:13:41 -0600

    Add robust test for decryption character set

--- test/decryption_test.rb
+++ test/decryption_test.rb
@@ -11,4 +11,12 @@ class DecryptionTest < Minitest::Test
   def test_it_exists
     assert_instance_of Decryption, @decryption
   end
-end 
+
+  def test_it_is_initialized_with_a_character_set
+    expected = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", " "]
+
+    assert_equal expected, @decryption.character_set
+    assert_equal 27, @decryption.character_set.count
+    assert_equal "a", @decryption.character_set[0]
+  end
+end
