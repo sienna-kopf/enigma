14d0769257a24800eb266a773e3ff17a1fc6cda5
Sienna Kopf <princess.kopf@gmail.com>
2020-06-08 16:27:23 -0600

    Add decrypt method, instance of decrypt in initialize, and require relative decryption file

--- lib/enigma.rb
+++ lib/enigma.rb
@@ -1,16 +1,26 @@
 require_relative './encryption'
+require_relative './decryption'
 require_relative './shifts'
 
 class Enigma
 
   def initialize
     @encryption = Encryption.new
+    @decryption = Decryption.new
     @shifts = Shifts.new
   end
 
   def encrypt(message, key, date)
     {
       :encryption => @encryption.encrypt_message(message, @shifts.shifts(key, date)),
+      :key => key,
+      :date => date
+    }
+  end
+
+  def decrypt(message, key, date)
+    {
+      :decryption => @decryption.decrypt_message(message, @shifts.shifts(key, date)),
       :key => key,
       :date => date
     }
