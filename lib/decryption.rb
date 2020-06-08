8ef77721bf4f7154359e0f0ebaec01f6c2c10965
Sienna Kopf <princess.kopf@gmail.com>
2020-06-07 23:24:34 -0600

    Reverse character set to effectivly decrypt a message with the same implementation as encrypt

--- lib/decryption.rb
+++ lib/decryption.rb
@@ -2,7 +2,7 @@ class Decryption
   attr_reader :character_set
 
   def initialize
-    @character_set = ("a".."z").to_a << " "
+    @character_set = (("a".."z").to_a << " ").reverse
   end
 
   def indexed_alphabet
@@ -17,5 +17,25 @@ class Decryption
 
   def alphabet_to_index
     indexed_alphabet.invert
+  end
+
+  def decrypt_message(encrypted_message, shifts)
+    decrypted_message = ""
+
+    encrypted_message.downcase.each_char.with_index do |character, i|
+      if @character_set.include?(character)
+        key = shifts[i % shifts.length]
+        alphabet_index = alphabet_to_index[character]
+
+        if (alphabet_index + key) < 27
+          decrypted_message << indexed_alphabet[alphabet_index + key]
+        else
+          decrypted_message << indexed_alphabet[(alphabet_index + key) % 27]
+        end
+      else
+        decrypted_message << character
+      end
+    end
+    decrypted_message
   end
 end
