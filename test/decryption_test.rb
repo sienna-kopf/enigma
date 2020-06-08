c3e12ddb1111d64eb5808f0b0ecd39402d0372c5
Sienna Kopf <princess.kopf@gmail.com>
2020-06-07 23:18:35 -0600

    Add test for decrypting a message

--- test/decryption_test.rb
+++ test/decryption_test.rb
@@ -29,4 +29,12 @@ class DecryptionTest < Minitest::Test
     expected = {"a"=>0, "b"=>1, "c"=>2, "d"=>3, "e"=>4, "f"=>5, "g"=>6, "h"=>7, "i"=>8, "j"=>9, "k"=>10, "l"=>11, "m"=>12, "n"=>13, "o"=>14, "p"=>15, "q"=>16, "r"=>17, "s"=>18, "t"=>19, "u"=>20, "v"=>21, "w"=>22, "x"=>23, "y"=>24, "z"=>25, " "=>26}
     assert_equal expected, @decryption.alphabet_to_index
   end
+
+  def test_it_can_decrypt_a_message
+    @shifts.stubs(:random_number).returns(2715)
+    Date.stubs(:today).returns(Date.new(1995, 8, 04))
+
+    assert_equal "keder ohulw", @decryption.decrypt_message("hello world", @shifts.shifts.values)
+    assert_equal "02715", @shifts.padded_random_number
+  end
 end
