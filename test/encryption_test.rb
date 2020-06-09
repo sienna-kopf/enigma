4b6bb9117dbc2c0df7b361f89ada7584282fc278
Sienna Kopf <princess.kopf@gmail.com>
2020-06-08 19:34:59 -0600

    Add assertions to index to alphabet test to increase robust-ness

--- test/encryption_test.rb
+++ test/encryption_test.rb
@@ -23,6 +23,8 @@ class EncryptionTest < Minitest::Test
   def test_it_can_create_an_index_to_letter_hash_for_character_set
     expected = {0=>"a", 1=>"b", 2=>"c", 3=>"d", 4=>"e", 5=>"f", 6=>"g", 7=>"h", 8=>"i", 9=>"j", 10=>"k", 11=>"l", 12=>"m", 13=>"n", 14=>"o", 15=>"p", 16=>"q", 17=>"r", 18=>"s", 19=>"t", 20=>"u", 21=>"v", 22=>"w", 23=>"x", 24=>"y", 25=>"z", 26=>" "}
     assert_equal expected, @encryption.index_to_alphabet
+    assert_equal 27, @encryption.index_to_alphabet.count
+    assert_equal " ", @encryption.index_to_alphabet[26]
   end
 
   def test_it_can_create_a_letter_to_index_hash_for_character_set
@@ -35,7 +37,7 @@ class EncryptionTest < Minitest::Test
     Date.stubs(:today).returns(Date.new(1995, 8, 04))
 
     assert_equal "keder ohulw", @encryption.crypt_message("hello world", @shifts.shifts)
-    assert_equal "02715", @shifts.padded_random_number
+    assert_equal "02715", @shifts.padded_number
   end
 
   def test_it_can_encrypt_a_message_that_contains_characters_outside_of_character_set
