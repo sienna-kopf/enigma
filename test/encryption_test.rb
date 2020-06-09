2aad0d2f7296a39069ecf57e83d5fd60c41bef14
Sienna Kopf <princess.kopf@gmail.com>
2020-06-08 19:36:40 -0600

    Add assertions to alphabet to index test to increase robust-ness

--- test/encryption_test.rb
+++ test/encryption_test.rb
@@ -20,7 +20,7 @@ class EncryptionTest < Minitest::Test
     assert_equal "a", @encryption.character_set[0]
   end
 
-  def test_it_can_create_an_index_to_letter_hash_for_character_set
+  def test_it_can_create_an_index_to_alphabet_hash_for_character_set
     expected = {0=>"a", 1=>"b", 2=>"c", 3=>"d", 4=>"e", 5=>"f", 6=>"g", 7=>"h", 8=>"i", 9=>"j", 10=>"k", 11=>"l", 12=>"m", 13=>"n", 14=>"o", 15=>"p", 16=>"q", 17=>"r", 18=>"s", 19=>"t", 20=>"u", 21=>"v", 22=>"w", 23=>"x", 24=>"y", 25=>"z", 26=>" "}
     assert_equal expected, @encryption.index_to_alphabet
     assert_equal 27, @encryption.index_to_alphabet.count
@@ -30,6 +30,8 @@ class EncryptionTest < Minitest::Test
   def test_it_can_create_a_letter_to_index_hash_for_character_set
     expected = {"a"=>0, "b"=>1, "c"=>2, "d"=>3, "e"=>4, "f"=>5, "g"=>6, "h"=>7, "i"=>8, "j"=>9, "k"=>10, "l"=>11, "m"=>12, "n"=>13, "o"=>14, "p"=>15, "q"=>16, "r"=>17, "s"=>18, "t"=>19, "u"=>20, "v"=>21, "w"=>22, "x"=>23, "y"=>24, "z"=>25, " "=>26}
     assert_equal expected, @encryption.alphabet_to_index
+    assert_equal 27, @encryption.alphabet_to_index.count
+    assert_equal 26, @encryption.alphabet_to_index[' ']
   end
 
   def test_it_can_encrypt_a_message
